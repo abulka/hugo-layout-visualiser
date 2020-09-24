0cbd9ab13a0d63635d1f2781006cd02da0b1bedf
Andy Bulka <abulka@gmail.com>
2020-09-24 13:51:21 +1000

    scan multiple themes

--- visualise.py
+++ visualise.py
@@ -13,7 +13,8 @@ previousEntries = []
 
 def process(htmlPath):
     uml = ""
-    fromFileName = os.path.basename(htmlPath)
+    # fromFileName = os.path.basename(htmlPath)
+    fromFileName = htmlPath
     fromFileName = os.path.splitext(fromFileName)[0]
     with open(htmlPath) as fp:
         lines = fp.readlines()
@@ -24,9 +25,16 @@ def process(htmlPath):
             m = re.search(partialRe, line)
             if m:
                 foundStr = m.group(1)
+                if len(foundStr.split(' ')) > 1:
+                    print(f"skipping difficult line {m.group(0)}")
+                    continue
                 partialFilenameNoExt = os.path.splitext(foundStr)[0]
                 if debug: print(f"✅ {line} / found match: {partialFilenameNoExt}")
-                entry = f'"{fromFileName}" - "{partialFilenameNoExt}"'
+                if fromFileName in ["single", "baseof", "list", "index"]:
+                    connector = "--->"
+                else:
+                    connector = "..>"
+                entry = f'"{fromFileName}" {connector} "{partialFilenameNoExt}"'
                 if entry not in previousEntries:
                     previousEntries.append(entry)
                     uml += f'{entry}\n'
@@ -37,25 +45,33 @@ def process(htmlPath):
 
 # Now recurse
 
+def scan(theme):
+    umls = ""
 
-umls = ""
-# umls += process(os.path.join(THEME_PATH, "ananke/layouts/_default/baseof.html"))
+    rootDir = os.path.join(THEME_PATH, f"{theme}/layouts/") + '/**/*.html'
+    for path in glob.iglob(rootDir, recursive=True):
+        if debug: print(path)
+        umls += process(path)
 
-rootDir = os.path.join(THEME_PATH, "ananke/layouts/") + '/**/*.html'
-for path in glob.iglob(rootDir, recursive=True):
-    if debug: print(path)
-    umls += process(path)
-
-finalPlantUML = f"""
+    finalPlantUML = f"""
 @startuml "test-uml"
 skinparam backgroundcolor Ivory/Azure
 
 {umls.rstrip()}
 
+class single << (S,#FF7700) _default >>
+class list << (L,#248811) _default >>
+class baseof << (B,orchid) >>
+class index << (I,yellow) >>
+
 @enduml
-"""
-# print(finalPlantUML)
-with open("out.wsd", "w") as fp:
-    fp.write(finalPlantUML.lstrip())
+    """
+    # print(finalPlantUML)
+    with open(f"out_{theme}.wsd", "w") as fp:
+        fp.write(finalPlantUML.lstrip())
+
+scan("ananke")
+scan("toha")
+scan("zzo")
 
 print("done")
