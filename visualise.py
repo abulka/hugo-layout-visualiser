a4ccbdb24bf4ffb011678cd5fb713b3801dc53c7
Andy Bulka <abulka@gmail.com>
2020-09-24 12:30:08 +1000

    recurse all html files

--- visualise.py
+++ visualise.py
@@ -1,50 +1,55 @@
+import glob
 import os
 import re
 from textwrap import dedent
 
 THEME_PATH = "/Users/Andy/Devel/hugo_tests/quickstart/themes"
-html_doc = os.path.join(THEME_PATH, "ananke/layouts/_default/baseof.html")
-
-# from bs4 import BeautifulSoup
-# with open(html_doc) as fp:
-#     soup = BeautifulSoup(fp, 'html.parser')
-# print(soup.prettify())
-# for link in soup.find_all("link"):
-#     print(link.get("href"))
 
 partialRe = r'.*[partial|partialCached]\s"(.*)"\s.*'
 finalPlantUML = ""
-uml = ""
+debug = False
 
-# back to basics
-with open(html_doc) as fp:
-    lines = fp.readlines()
 
-for line in lines:
-    if "partial" in line:
-        line = line.strip()
-        m = re.search(partialRe, line)
-        if m:
-            foundStr =m.group(1)
-            partialFilenameNoExt = os.path.splitext(foundStr)[0]
-            msg = f"✅ {line} / found match: {partialFilenameNoExt}"
-        else:
-            msg = f"❌ {line} / no match?"
-        print(msg)
-        uml += f'"baseof" - "{partialFilenameNoExt}"\n'
+def process(htmlPath):
+    uml = ""
+    fromFileName = os.path.basename(htmlPath)
+    fromFileName = os.path.splitext(fromFileName)[0]
+    with open(htmlPath) as fp:
+        lines = fp.readlines()
 
-# uml = "X\n\tY"
-uml = uml.rstrip()
+    for line in lines:
+        if "partial" in line:
+            line = line.strip()
+            m = re.search(partialRe, line)
+            if m:
+                foundStr = m.group(1)
+                partialFilenameNoExt = os.path.splitext(foundStr)[0]
+                if debug: print(f"✅ {line} / found match: {partialFilenameNoExt}")
+                uml += f'"{fromFileName}" - "{partialFilenameNoExt}"\n'
+            else:
+                if debug: print(f"❌ {line} / no match?")
+    return uml
+
+
+# Now recurse
+
+umls = ""
+# umls += process(os.path.join(THEME_PATH, "ananke/layouts/_default/baseof.html"))
+
+rootDir = os.path.join(THEME_PATH, "ananke/layouts/") + '/**/*.html'
+for path in glob.iglob(rootDir, recursive=True):
+    if debug: print(path)
+    umls += process(path)
+
 finalPlantUML = f"""
 @startuml "test-uml"
 skinparam backgroundcolor Ivory/Azure
 
-{uml}
+{umls}
 
 @enduml
 """
-
-print(finalPlantUML)
+# print(finalPlantUML)
 with open("out.wsd", "w") as fp:
     fp.write(finalPlantUML)
 
